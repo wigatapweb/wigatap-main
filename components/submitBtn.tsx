296c681c5c8abda117cba59a01541210184cd2ae
myspace8 <comfyspace8@gmail.com>
2023-02-28 00:59:11 -0800

    fixes

--- components/submitBtn.tsx
+++ components/submitBtn.tsx
@@ -1,10 +1,8 @@
-import Link from 'next/link'
-import { MouseEventHandler } from 'react'
 
-const SubmitBtn = (func: MouseEventHandler<HTMLButtonElement> | undefined) => {
+const SubmitBtn = () => {
   return (
     <div>
-    <button type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" className="inline-block px-6 py-2.5 mb-6 w-full bg-green-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg transition duration-150 ease-in-out" onClick={func}>Send</button>
+    <button type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" className="inline-block px-6 py-2.5 mb-6 w-full bg-green-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg transition duration-150 ease-in-out">Send</button>
     </div>
   )
 }
