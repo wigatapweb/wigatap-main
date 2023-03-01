870eb79b738ef081faf54ef5aca54513461aa6e0
Joe <97924558+myspace8@users.noreply.github.com>
2023-03-01 14:32:07 +0000

    ..

--- components/about.tsx
+++ components/about.tsx
@@ -5,14 +5,14 @@ const About = () => {
   return (
     <div>
     <h2 className="mb-4 mt-8 text-center text-5xl md:text-7xl font-bold tracking-tighter leading-tight">
-      About Us
+      Founder's Story
     </h2>
     <div className="flex flex-wrap justify-center">
     <img
         src="founder.JPG"
-        className="h-auto max-full rounded-lg shadow-none transition-shadow duration-300 ease-in-out hover:shadow-lg hover:shadow-black/30"
+        className="h-auto w-full rounded-lg shadow-none transition-shadow duration-300 ease-in-out hover:shadow-lg hover:shadow-black/30"
         alt="" />
-        <em></em>
+        <em className='text-sm text-slate-500'>Rev Gyima Boafo</em>
     </div>
     <p className='text-center mt-4'>At the outset, the primary vision of the charity foundation was to extend a helping hand to the less privileged and orphaned children by providing them with access to educational resources. The ultimate goal was to address the significant disparities in education in Ghana and promote a more equitable distribution of opportunities for young people. The foundation recognized the critical role that access to educational materials plays in this regard, as it can significantly influence students' academic performance and future prospects.</p>
     <p className='text-center mt-2'>As the foundation's work has progressed, it has become increasingly clear that there is a broader need to support the development of the whole child, beyond just their academic pursuits. Therefore, the foundation has evolved its vision to encompass a more holistic approach to supporting children's growth and development. This expanded vision includes promoting and nurturing children's natural talents and potentials by providing them with the necessary tools and resources to explore and develop their interests fully.</p>
